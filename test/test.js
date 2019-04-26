67b234a960f211aed71fbce1c1074bf5888f2cd9
Sidhant Chadda <sidhantchadda@gmail.com>
2019-04-26 01:49:14 -0400

    added test cases

--- test/test.js
+++ test/test.js
@@ -29,60 +29,45 @@ var options = {
 };
 
 describe('tests', function(){
-  // can nest more describe()'s here, or tests go here
-  it('findByTeamName', function(){
+  it('findByTeamName', function(done){
     client.connect(function(err, db) {
       const dbase = client.db(dbName);
       Database.findByTeamName(dbase, team_stats, 'Warriors', function(docs) {
-        // console.log(docs[0].teamName)
         id = docs[0]._id;
         assert.equal(docs[0].teamName, 'Warriors');
+        done();
       });
-      //process.exit();
     });
   });
-  it('getPlayers', function(){
-    client.connect(function(err, db) {
-      // console.log("HERE YOU FUCKBOY\n\n\n")
-      const dbase = client.db(dbName);
-      Database.getPlayers(dbase, player_stats, function(players) {
-        // console.log(players[0].playerName);
-        // console.log(players[0].f_name);
-        // console.log(players[0].l_name);
-        // console.log(players[0].f_name + '_' + players[0].l_name);
-        assert.equal(players[0].f_name + '_' + players[0].l_name, players[0].playerName);
-      });
-      //process.exit();
-    });
-  });
-  it('getTeamPredictions', function(){
+  // it('getPlayers', function(done){
+  //   client.connect(function(err, db) {
+  //     const dbase = client.db(dbName);
+  //     Database.getPlayers(dbase, player_stats, function(players) {
+  //       assert.equal(players[0].f_name + '_' + players[0].l_name, players[0].playerName);
+  //       done();
+  //     });
+  //   });
+  // }); //uncomment this part out
+  it('getTeamPredictions', function(done){
     client.connect(function(err, db) {
 
       const dbase = client.db(dbName);
       Database.findByTeamName(dbase, team_stats, 'Warriors', function(docs) {
-        console.log("HEREEEEE\n\n")
         Database.getTeamPredictions(dbase, team_predictions, docs[0]._id, function(predicts) {
-          // console.log(predicts[0].full_name.includes("Warriors"));
 
           assert.equal(predicts[0].full_name.includes("Warriors"), true)
-
+          done();
 
         });
       });
     });
   });
 
-  /////////////////////
-  it('getPredictions', function(){
+  it('getPredictions', function(done){
     client.connect(function(err, db) {
-
       const dbase = client.db(dbName);
       Database.findByTeamName(dbase, team_stats, 'Warriors', function(docs) {
         Database.getPredictions(dbase, team_predictions, function(docs) {
-        // console.log(players[0].playerName);
-        // console.log(players[0].f_name);
-        // console.log(players[0].l_name);
-        // console.log(players[0].f_name + '_' + players[0].l_name);
         var j = -1;
         for(var i = 0; i < docs.length; i++){
           if(docs[i].full_name.includes("Warriors")){
@@ -94,12 +79,44 @@ describe('tests', function(){
         if(j > -1){
           // console.log(docs[j].full_name)
           assert.equal(docs[j].full_name.includes("Warriors"), true);
+          done();
         }
         });
       //process.exit();
       });
     });
   });
+
+  //betting odds functionality
+  const odds = require('../common/betting');
+  it('getBettingOdds', function(done) {
+    odds.getBettingOdds(client, function(docs) {
+      //check the length
+      assert.equal(docs.length != 0, true);
+      done();
+    });
+  });
+
+  it('bettingOddsValues', function(done) {
+    odds.getBettingOdds(client, function(docs) {
+      //has the correct value types
+      const team = docs[0];
+      assert.equal(team["team1"] != undefined, true);
+      assert.equal(team["team2"] != undefined, true);
+      assert.equal(team["val1"] != undefined, true);
+      assert.equal(team["val2"] != undefined, true);
+      assert.equal(Number.isInteger(team["val1"]), true);
+      assert.equal(Number.isInteger(team["val2"]), true);
+      done();
+    });
+  });
+
+  //date parsing functionality
+  // const Dates = require('../common/date');
+  // it('isTodayToday', function(done) {
+    
+  // });
+
   // it('getBoxScores', function(){
   //   client.connect(function(err, db) {
   //
@@ -126,4 +143,5 @@ describe('tests', function(){
   //     //process.exit();
   //   });
   // });
+
 });
