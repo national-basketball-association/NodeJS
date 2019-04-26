baeb69efc98b36e692ee6a481ee359b61919ec6f
Sidhant Chadda <sidhantchadda@gmail.com>
2019-04-26 01:07:27 -0400

    added unit testing framework to backend

---
+++ test/run-tests.bash
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# if any command inside script returns error, exit and return that error 
+set -e
+
+# magic line to ensure that we're always inside the root of our application,
+# no matter from which directory we'll run script
+# thanks to it we can just enter `./scripts/run-tests.bash`
+cd "${0%/*}/.."
+
+#echo "installing dependencies"
+#npm i
+echo "Running tests"
+echo "Make sure dependencies are installed!"
+npm test
+
+echo "............................" 
+echo "Succesful!" && exit 0
